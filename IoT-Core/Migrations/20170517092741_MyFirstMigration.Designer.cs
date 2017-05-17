87b7fd2e4e658babd09ffeef7331fb3e1ff775f7
René Schimmelpfennig <mail@reneschimmelpfennig.de>
2017-05-17 11:59:28 +0200

    init commit

---
+++ IoT-Core/Migrations/20170517092741_MyFirstMigration.Designer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using IoT_Core.Models;
+
+namespace IoT_Core.Migrations
+{
+    [DbContext(typeof(SensorValueContext))]
+    [Migration("20170517092741_MyFirstMigration")]
+    partial class MyFirstMigration
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.2");
+
+            modelBuilder.Entity("IoT_Core.Models.SensorValues", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTime>("Created");
+
+                    b.Property<DateTime>("DeviceTime");
+
+                    b.Property<byte>("Humidity");
+
+                    b.Property<int>("SoilMoisture");
+
+                    b.Property<int>("Temperature");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("SensorValues");
+                });
+        }
+    }
+}
---
+++ IoT-Core/Models/SensorValues.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IoT_Core.Models
+{
+    public class SensorValues
+    {
+        public int Id { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime DeviceTime { get; set; }
+        public int Temperature { get; set; }
+        public byte Humidity { get; set; }
+        public int SoilMoisture { get; set; }
+    }
+}
