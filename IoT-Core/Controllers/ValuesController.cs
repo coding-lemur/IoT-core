0577fff643c1aa3c6f567bd50f22e8e399f9da43
René Schimmelpfennig <rene.schimmelpfennig@cornelsen.de>
2017-05-17 15:37:14 +0200

    implement GET and POST in controller implement bad request for PUT and DELETE

--- IoT-Core/Controllers/ValuesController.cs
+++ IoT-Core/Controllers/ValuesController.cs
@@ -3,42 +3,72 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using IoT_Core.Models;
 
 namespace IoT_Core.Controllers
 {
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
+        private SensorValueContext _dbContext;
+
+        public ValuesController(SensorValueContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
         // GET api/values
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IActionResult Get()
         {
-            return new string[] { "value1", "value2" };
+            var results = _dbContext.SensorValues
+                .OrderBy(sv => sv.Id)
+                .ToList();
+            return Ok(results);
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public IActionResult Get(int id)
         {
-            return "value";
+            var sensorValues = _dbContext.SensorValues
+                .FirstOrDefault(value => value.Id == id);
+
+            if (sensorValues == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(sensorValues);
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody]string value)
+        public async Task<IActionResult> Post([FromBody]SensorValues values)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            _dbContext.SensorValues.Add(values);
+            await _dbContext.SaveChangesAsync();
+
+            return CreatedAtAction("Get", values.Id);
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        public IActionResult Put(int id, [FromBody]string value)
         {
+            return Forbid();
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(int id)
         {
+            return Forbid();
         }
     }
 }
