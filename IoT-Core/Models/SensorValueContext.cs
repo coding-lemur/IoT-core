d22dfe40d0a0d38e24ce4e47d8a455395d5d0337
René Schimmelpfennig <rene.schimmelpfennig@cornelsen.de>
2017-05-17 11:59:28 +0200

    init commit

---
+++ IoT-Core/Models/SensorValueContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+
+namespace IoT_Core.Models
+{
+    public class SensorValueContext : DbContext
+    {
+        public DbSet<SensorValues> SensorValues { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=values.db");
+        }
+    }
+}
