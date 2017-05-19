3da9656ff0709dbeedafb9339502501528bdccbe
René Schimmelpfennig <rene.schimmelpfennig@cornelsen.de>
2017-05-19 16:26:31 +0200

    implement controller for returning current datetime in special format for the Arduino system

---
+++ IoT-Core/Controllers/DateTimeController.cs
@@ -0,0 +1,25 @@
+﻿using IoT_Core.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IoT_Core.Controllers
+{
+    [Route("api/[controller]")]
+    public class DateTimeController
+    {
+        [HttpGet]
+        public RtcDateTime Get()
+        {
+            DateTime now = DateTime.Now;
+
+            return new RtcDateTime
+            {
+                Date = now.ToString("MMM dd yyyy"),
+                Time = now.ToString("HH:mm:ss")
+            };
+        }
+    }
+}
---
+++ IoT-Core/Models/RtcDateTime.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IoT_Core.Models
+{
+    public class RtcDateTime
+    {
+        public String Date { get; set; }
+
+        public String Time { get; set; }
+    }
+}
