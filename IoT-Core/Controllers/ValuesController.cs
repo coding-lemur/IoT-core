c09d43cd7ed34e7129ba3dd8b2dbb9832e27a3db
René Schimmelpfennig <mail@reneschimmelpfennig.de>
2017-05-20 22:41:49 +0200

    remove unused actions in Values controller

--- IoT-Core/Controllers/ValuesController.cs
+++ IoT-Core/Controllers/ValuesController.cs
@@ -56,19 +56,5 @@ namespace IoT_Core.Controllers
 
             return CreatedAtAction("Get", values.Id);
         }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public IActionResult Put(int id, [FromBody]string value)
-        {
-            return Forbid();
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public IActionResult Delete(int id)
-        {
-            return Forbid();
-        }
     }
 }
