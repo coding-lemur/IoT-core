92680a95e629a571dee052388ee14321e99db4ae
René Schimmelpfennig <mail@reneschimmelpfennig.de>
2017-05-26 09:45:19 +0200

    regenerating database migrations

---
+++ IoT-Core/Migrations/DataContextModelSnapshot.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using IoT_Core.Models;
+
+namespace IoT_Core.Migrations
+{
+    [DbContext(typeof(DataContext))]
+    partial class DataContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.2");
+
+            modelBuilder.Entity("IoT_Core.Models.SensorValues", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTime>("Date")
+                        .ValueGeneratedOnAdd()
+                        .HasDefaultValueSql("datetime('now','localtime')");
+
+                    b.Property<int?>("DeviceTimestamp");
+
+                    b.Property<float>("Humidity");
+
+                    b.Property<int>("SoilMoisture");
+
+                    b.Property<float>("Temperature");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Sensors");
+                });
+
+            modelBuilder.Entity("IoT_Core.Models.WateringValue", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTime>("Date")
+                        .ValueGeneratedOnAdd()
+                        .HasDefaultValueSql("datetime('now','localtime')");
+
+                    b.Property<int>("Milliseconds");
+
+                    b.Property<int?>("SensorsId");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("SensorsId");
+
+                    b.ToTable("Watering");
+                });
+
+            modelBuilder.Entity("IoT_Core.Models.WateringValue", b =>
+                {
+                    b.HasOne("IoT_Core.Models.SensorValues", "Sensors")
+                        .WithMany()
+                        .HasForeignKey("SensorsId");
+                });
+        }
+    }
+}
