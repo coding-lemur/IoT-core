01793380dfc21d4cf6d872fb1d204db6c9da48da
René Schimmelpfennig <mail@reneschimmelpfennig.de>
2017-06-16 13:36:37 +0200

    set Date fields for SensorValues and WateringValue models to current datetime

--- IoT-Core/Models/WateringValue.cs
+++ IoT-Core/Models/WateringValue.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,10 +10,16 @@ namespace IoT_Core.Models
     {
         public int Id { get; set; }
 
+        [BsonDateTimeOptions(Kind = DateTimeKind.Local)]
         public DateTime Date { get; set; }
 
         public int Milliseconds { get; set; }
 
         public SensorValues Sensors { get; set; }
+
+        public WateringValue()
+        {
+            Date = DateTime.Now;
+        }
     }
 }
